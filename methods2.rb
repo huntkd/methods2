7abc1552b6ab502e0edcbebc8b3e1ddbdd5be758
huntkd <32105448+huntkd@users.noreply.github.com>
2017-09-26 11:14:17 -0600

    is the party good or bad function

--- methods2.rb
+++ methods2.rb
@@ -16,10 +16,55 @@
 
 
 # TODO - write elevenish?
+def elevenish (n)
+	if n == 11
+		return true
+	end
+
+	if n%11
+		return true
+	end
+
+end
 	
 # TODO - write ice_cream_party?
 
+def ice_cream_party (ice_cream, candy)
+	if ice_cream < 5
+		return 0
+	end
+	if candy < 5
+		return 0
+	end
+	if candy >= (ice_cream * 2)
+		return 2
+	end
+	if ice_cream >= (candy * 2)
+		return 2
+	end
+	if ice_cream >= 5
+		if candy >= 5
+			return 1
+		end
+	end
+
+end
+
 # TODO - write successful_squirrel_party?
+
+def successful_squirrel_party (nuts, is_weekend)
+	if is_weekend 
+		if nuts >= 40
+			return true
+		end
+	end
+	if !is_weekend 
+		if nuts > 60 
+			return false
+		end
+	end
+
+end
 
 # TODO - write ticket
 
